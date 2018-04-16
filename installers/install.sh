dad1c162c0df8fb2b53b8938553eb0f8bb0f0db1
Alex Godwin <alexkgodwin@gmail.com>
2018-04-16 15:01:55 -0400

    Updated variable structure

--- installers/install.sh
+++ installers/install.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
-# Platform detection
+# Variables
+Artifact=
+Latest=
+RepoLink='https://api.github.com/repos/gladiusio/gladius-node/releases/latest'
+ReleaseEndpoint='https://github.com/gladiusio/gladius-node/releases/download'
 Platform='unknown'
-unamestr = $(uname)
+Architecture=
+unamestr=$(uname)
+MachineType=$(uname -m)
+
+# Platform detection
 if [[ "$unamestr" == 'Linux' ]]; then
-   Platform='linux'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-   Platform='freebsd'
+  Platform='linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  Platform='macos'
 fi
 
-MachingType=`uname -m`
-Architecture=''
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+# Detect the architecture
+if [ $MachineType == 'x86_64' ]; then
   Architecture='amd64'
 else
   Architecture='386'
 fi
 
-Latest=get_latest_release
+# Get the latest release of the gladius-node
+Latest=$(curl --silent "$RepoLink" | # Get latest release from GitHub api
+  grep '"tag_name":' |               # Get tag line
+  sed -E 's/.*"([^"]+)".*/\1/'       # Pluck JSON value
+)
+
 
 # Set the correct variables based on the detected platform
 if [[ $Platform == 'linux' ]]; then
-  Artifact="https://github.com/gladiusio/gladius-node/releases/download/$Latest/linux_${Architecture}.zip"
-elif [[ $Platform == 'darwin' ]]; then
-  Artifact="https://github.com/gladiusio/gladius-node/releases/download/$Latest/macOS_${Architecture}.zip"
+  Artifact="$ReleaseEndpoint/$Latest/linux_$Architecture.zip"
+elif [[ $Platform == 'macos' ]]; then
+  Artifact="$ReleaseEndpoint/$Latest/macOS_$Architecture.zip"
 else
   echo "Unknown platform... See instructions for manual install on http://github.com/gladiusio/gladius-node"
   exit 1
 fi
 
-
-# Get the latest release of the gladius-node
-get_latest_release() {
-  curl --silent "https://api.github.com/repos/gladiusio/gladius-node/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
-}
+echo $Artifact
