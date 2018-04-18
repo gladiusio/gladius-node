cd5a28e26b9640d3b0d0d2f46d447de2c0753100
Celo44 <marcelomcandrew@gmail.com>
2018-04-18 14:14:41 -0400

    all endpoints from the nodejs control daemon are implemented and working

--- cmd/gladius-cli/cli.go
+++ cmd/gladius-cli/cli.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gladiusio/gladius-node/pkg/cli"
 )
 
-func main() {
-	var localSettings = node.Settings{}
-	var myNode = node.Node{}
+var localSettings = node.Settings{}
+var myNode = node.Node{}
+var poolAddress = "0xC88a29cf8F0Baf07fc822DEaA24b383Fc30f27e4"
 
-	myNode.Data.Name = "celo-test-2"
+func main() {
+
+	theWorks()
+
+}
+
+func theWorks() {
+
+	myNode.Data.Name = "celo-test-3"
 	myNode.Data.Email = "celo@gladius.io"
 	myNode.Data.IPAddress = "1.1.1.1"
 	myNode.Data.Status = "active"
@@ -17,26 +27,28 @@ func main() {
 	node.SetSettings("ropsten", &localSettings)
 	node.PostSettings(&localSettings)
 
-	// // create a node contract and wait for tx to finish
-	// tx := node.CreateNode()
-	// node.WaitForTx(tx)
-	//
-	// // get the address of new node, set its data, and wait
-	// nodeAddress := node.GetNodeAddress()
-	// tx = node.SetNodeData(nodeAddress, myNode)
-	// node.WaitForTx(tx)
-	//
-	// // apply to pool
-	// tx = node.ApplyToPool(nodeAddress, "0xC88a29cf8F0Baf07fc822DEaA24b383Fc30f27e4")
-	// node.WaitForTx(tx)
-	//
-	// node.StartEdgeNode()
-	// node.StopEdgeNode()
-	// node.StatusEdgeNode()
+	// create a node contract and wait for tx to finish
+	tx := node.CreateNode()
+	node.WaitForTx(tx)
+	fmt.Println("Node created!")
 
-	// test()
-	// getSettings()
-	// fmt.Println(getNodeAddress())
-	// res := createNode()
-	// checkTx("0x3e1514aa08b0dcf9058893fef05c15aae6986ec64eddcbf0f5b30b9694233e73")
+	// get the address of new node, set its data, and wait
+	myNode.Address = node.GetNodeAddress()
+	tx = node.SetNodeData(myNode.Address, myNode)
+	node.WaitForTx(tx)
+	fmt.Println("Data set!")
+
+	// apply to pool
+	tx = node.ApplyToPool(myNode.Address, poolAddress)
+	node.WaitForTx(tx)
+	fmt.Println("Applied to pool!")
+
+	// check pool application status
+	fmt.Println(node.CheckPoolApplication(myNode.Address, poolAddress))
+}
+
+func networkDaemon() {
+	node.StartEdgeNode()
+	node.StatusEdgeNode()
+	node.StopEdgeNode()
 }
