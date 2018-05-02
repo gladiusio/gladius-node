56dfebc6f4e7f1d185391f1498ffc48e98a524ae
Alex Godwin <alexkgodwin@gmail.com>
2018-05-02 13:32:26 -0400

    Updated calling method

--- cmd/gladius-networkd/main.go
+++ cmd/gladius-networkd/main.go
@@ -2,22 +2,9 @@ package main
 
 import (
 	"github.com/gladiusio/gladius-networkd/networkd"
-	"github.com/gladiusio/gladius-utils/init/manager"
-
-	"github.com/gladiusio/gladius-utils/config"
 )
 
-// Main entry-point for the service
+// Main - entry-point for the service
 func main() {
-	// Setup config handling
-	config.SetupConfig("gladius-networkd", config.NetworkDaemonDefaults())
-
-	// Define some variables
-	name, displayName, description :=
-		"GladiusNetworkDaemon",
-		"Gladius Network (Edge) Daemon",
-		"Gladius Network (Edge) Daemon"
-
-	// Run the function "run" in newtworkd as a service
-	manager.RunService(name, displayName, description, networkd.Run)
+	networkd.SetupAndRun()
 }
