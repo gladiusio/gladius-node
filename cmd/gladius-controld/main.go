25c703bd036d49a33c616bd8da153295a6e45a20
Nathan Grey <nate@studioember.com>
2018-08-22 13:44:09 -0400

    Uses new initialization syntax

--- cmd/gladius-controld/main.go
+++ cmd/gladius-controld/main.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-	"github.com/gladiusio/gladius-controld/pkg/config"
-	"github.com/gladiusio/gladius-controld/pkg/routing"
-	"github.com/gladiusio/gladius-utils/init/manager"
+	"github.com/gladiusio/gladius-controld/controld"
 )
 
 func main() {
-	// Define some variables
-	name, displayName, description := config.Config()
-
-	router := config.NodeRouter()
-
-	cRouter := routing.ControlRouter{
-		Router: router,
-		Port:   "3001",
-	}
-
-	// Run the function "run" in newtworkd as a service
-	manager.RunService(name, displayName, description, cRouter.Start)
+	controld.InitializeNodeManager()
 }
