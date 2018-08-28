98d2f568a3d959962bb4a06304182b67cc7521d5
Alex Godwin <alexkgodwin@gmail.com>
2018-08-28 14:07:42 -0400

    Started go tests additons

---
+++ tests/local/p2p_test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"testing"
+)
+
+const (
+	numToTest        = 10
+	controlStartPort = 3001
+	contentStartPort = 8080
+	p2pStartPort     = 7946
+)
+
+func TestP2P(t *testing.T) {
+	tester := newP2PTester(numToTest, t)
+
+	tester.createGladiusBases()
+	tester.startDaemons()
+
+	tester.stopDaemons()
+	tester.deleteGladiusBases()
+}
+
+type p2pTester struct {
+	numOfNodes   int        // How many nodes to start
+	t            *testing.T // So we can run tests
+	contentPorts []int      // The range of networkd ports
+	controlPorts []int      // The range of controld ports
+	p2pPorts     []int      // P2P port range
+	contentPIDs  []int      // Networkd PIDs (so we can kill them)
+	controlPIDs  []int      // Cotnrold PIDs (so we can kill them)
+}
+
+// Take in a testing object and a number of nodes and create a tester
+func newP2PTester(numOfNodes int, t *testing.T) *p2pTester {
+	if numOfNodes < 2 {
+		t.Error("Need at least 2 nodes to run test!")
+	}
+
+	// Create a tester to return
+	tester := &p2pTester{
+		t:            t,
+		numOfNodes:   numOfNodes,
+		contentPIDs:  make([]int, numOfNodes),
+		controlPIDs:  make([]int, numOfNodes),
+		contentPorts: make([]int, numOfNodes),
+		p2pPorts:     make([]int, numOfNodes),
+		controlPorts: make([]int, numOfNodes),
+	}
+
+	// Setup our ports
+	for i := 0; i < numOfNodes; i++ {
+		tester.contentPorts[i] = contentStartPort + i
+		tester.controlPorts[i] = controlStartPort + i
+		tester.p2pPorts[i] = p2pStartPort + i
+	}
+
+	return tester
+}
+
+// Create enough base direcories for all of the nodes in the network
+func (pt *p2pTester) createGladiusBases() {
+	for i := 0; i < pt.numOfNodes; i++ {
+		err := os.MkdirAll("./bases/g"+strconv.Itoa(i), os.ModePerm)
+		if err != nil {
+			pt.t.Error(err)
+		}
+	}
+}
+
+// Clean the whole "./bases directory"
+func (pt *p2pTester) deleteGladiusBases() {
+	for i := 0; i < pt.numOfNodes; i++ {
+		err := os.Remove("./bases/g" + strconv.Itoa(i))
+		if err != nil {
+			pt.t.Error(err)
+		}
+	}
+}
+
+// Start the daemons
+func (pt *p2pTester) startDaemons() {
+	for i := 0; i < pt.numOfNodes; i++ {
+	}
+}
+
+// Stop the daemons
+func (pt *p2pTester) stopDaemons() {
+	for i := 0; i < pt.numOfNodes; i++ {
+	}
+}
--- tests/vagrant/mininet/README.md
+++ tests/vagrant/mininet/README.md
@@ -1,2 +1,3 @@
-These are pretty broken, mininet needs to be swapped for something else due to
-issues with how it creates private directories.
+**Warning:** These are pretty broken, mininet needs to be swapped for something else due to
+issues with how it creates private directories. They are very inconsistent and sometimes share files between hosts. May end up creating
+a docker system.
