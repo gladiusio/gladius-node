11f0c68424cb4d5f3d804d70f43586de512f8ae9
Alex Godwin <alexkgodwin@gmail.com>
2019-01-03 12:53:41 -0500

    Updated build

--- scripts/build_osx.sh
+++ scripts/build_osx.sh
@@ -4,5 +4,5 @@
 
 LDFLAGS="" TARGET=/build/gladius SOURCE=/src/gladius-cli/cmd/main.go PROJECT_ROOT=/src/gladius-cli /scripts/go_osx.sh
 LDFLAGS="" TARGET=/build/gladius-edged SOURCE=/src/gladius-edged/cmd/gladius-edged/main.go PROJECT_ROOT=/src/gladius-edged /scripts/go_osx.sh
-LDFLAGS="" TARGET=/build/gladius-network-gateway SOURCE=/src/gladius-network-gateway/cmd/main.go PROJECT_ROOT=/src/gladius-cli /scripts/go_osx.sh
+LDFLAGS="" TARGET=/build/gladius-network-gateway SOURCE=/src/gladius-network-gateway/cmd/main.go PROJECT_ROOT=/src/gladius-network-gateway /scripts/go_osx.sh
 LDFLAGS="" TARGET=/build/gladius-guardian SOURCE=/src/gladius-guardian/main.go PROJECT_ROOT=/src/gladius-guardian /scripts/go_osx.sh
